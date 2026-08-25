f2c72a4d153633a81ef034805a56546fa41a20ce
Luis Reyes Caro <luisrafaelreyescaro1@gmail.com>
2026-08-24 20:53:22 -0500

    Update db.py

--- backend/api/config/db.py
+++ backend/api/config/db.py
@@ -1,2 +1,18 @@
+from sqlmodel import Field, SQLModel, create_engine
+
 document = "db.json"
 fake_db = []
+
+class user(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    email: str
+    password: str
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+SQLModel.metadata.create_all(engine)
